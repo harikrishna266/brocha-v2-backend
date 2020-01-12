892191607f84d90ae40bbda8e0ab0f8ccd4fb130
hari <hkrishna@taxbackinternational.com>
2020-01-12 19:47:05 +0530

    chamged directory

--- fbx/models.py
+++ fbx/models.py
@@ -6,10 +6,10 @@ from django.db import models
 
 class Fbx(models.Model):
     name = models.CharField(max_length=200)
-    fbx = models.FileField(max_length=200, blank=True,upload_to='images/designs')
-    normals = models.FileField(max_length=200, blank=True,upload_to='images/designs')
-    bump = models.FileField(max_length=200, blank=True,upload_to='images/designs')
-    texture = models.FileField(max_length=200, blank=True,upload_to='images/designs')
+    fbx = models.FileField(max_length=200, blank=True,upload_to='static/designs')
+    normals = models.FileField(max_length=200, blank=True,upload_to='static/designs')
+    bump = models.FileField(max_length=200, blank=True,upload_to='static/designs')
+    texture = models.FileField(max_length=200, blank=True,upload_to='static/designs')
     status = models.BooleanField(default=False)
     pub_date = models.DateTimeField(auto_now=True)
 
