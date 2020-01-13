b462f1475837c3b02cb750f83c07a33c08d5023a
Ubuntu <ubuntu@ip-172-31-43-56.ap-south-1.compute.internal>
2020-01-13 07:39:47 +0000

    setup ready

--- brochav2/settings.py
+++ brochav2/settings.py
@@ -121,8 +121,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+MEDIA_ROOT = os.path.join(BASE_DIR,STATIC_ROOT,'media')
+MEDIA_URL = '/media/'
+#STATIC_URL = '/static/'
+
+#STATIC_ROOT = os.path.join(BASE_DIR, '/var/www/html/brocha-v2-backend/static/')
+#print ( STATIC_ROOT)
 
